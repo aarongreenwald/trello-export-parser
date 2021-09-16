0105fb11e883c7c7f0618ff1c55e1c49a66a4929
Aaron Greenwald <aaron@aarongreenwald.com>
2021-09-16 21:07:51 +0300

    more functionality, give each card its own file if there's content

--- index.js
+++ index.js
@@ -38,42 +38,75 @@ const writeBoard = async ({board, content}) => {
 
 const writeList = async (board, list) => {
     //TODO consider including the list index for sort position. How often is it meaningful?
-    console.log('writing', board.org, list.boardSlug)
-
     const listPath = `${OUTPUT}/${board.org}/${list.boardSlug}/${list.closed ? '.' : ''}${list.name}`;
-    await fs.mkdir(`${OUTPUT}/${board.org}/${list.boardSlug}`, { recursive: true });
-    await fs.writeFile(listPath, list.desc ? list.desc : '')
+    console.log('Writing list', listPath)
+    await fs.mkdir(listPath, { recursive: true });
+    if (!list.cards.length) {
+        return
+    }
+    const listGeneralPath = `${listPath}/${'0'.repeat(Math.floor(Math.log10(list.cards.length) + 1))}-general`;
+    await fs.writeFile(listGeneralPath, list.desc ? list.desc : '')
     for (let i = 0; i < list.cards.length; i++) {
         const c = list.cards[i];
-        await fs.appendFile(listPath, c.name)
-        if (c.body) {
-            await fs.appendFile(listPath, '\n')
-            await fs.appendFile(listPath, '~~~~~~~~~~~~~~~~~~~~\n')
-            //TODO determine if card should be extracted to its own file based on depth of content
-            //How to mix card files with list files?? When should lists be files and when should they be directories?
-            //consider making all cards files, adding a "NoContent" tag and ordering the files
-            await fs.appendFile(listPath, c.body)
-        }
 
-        if (c.links.length) {
-            await fs.appendFile(listPath, '\nLinks:\n')
-            for (let j = 0; j < c.links.length; j++) {
-                const link = c.links[j]
-                if (link.external) {
-                    await fs.appendFile(listPath, `* [${link.name}](${link.url})`)
-                } else {
-                    await fs.appendFile(listPath, `* [${link.name}](./${link.name})`)
-                    console.warn('Need to download', `${OUTPUT}/${board.org}/${list.boardSlug}/${list.name}/${c.slug}/${link.name}`, link.url)
-                    // const stream = require('fs').createWriteStream(`${OUTPUT}/${board.org}/${list.boardSlug}/${link.name}`)
-                    // require('https').get(link.url, res => res.pipe(stream))
+        await fs.appendFile(listGeneralPath, `### ${c.name}${c.closed ? ' (Archived)' : ''}\n`)
+        await fs.appendFile(listGeneralPath, `_Migrated from Trello, last modified: ${c.modified}_\n`)
+        if (c.body || c.links.length) {
+            await fs.appendFile(listGeneralPath, `\nSee [${c.slug}](./${c.slug})\n`)
+        }
+        await fs.appendFile(listGeneralPath, '\n-------------------\n\n')
+
+        if (c.body || c.links.length || c.checklists.length) {
+            const cardPath = `${listPath}/${c.slug}`
+            await fs.appendFile(cardPath, `_Migrated from Trello, last modified: ${c.modified}_\n\n`)
+
+            if (c.body) {
+                await fs.appendFile(cardPath, c.body)
+            }
+
+            if (c.checklistIds.length !== c.checklists.length) {
+                throw new Error('wat')
+            }
+
+            for (let j = 0; j < c.checklists.length; j++){
+                const chlist = c.checklists[j];
+                await fs.appendFile(cardPath, `\n\n# ${chlist.name}\n\n`)
+                for (let k = 0; k < chlist.items.length; k++) {
+                    const item = chlist.items[k];
+                    if (item.due) {
+                        throw new Error('Unhandled due date')
+                    }
+                    await fs.appendFile(cardPath, `[${item.state === 'complete' ? 'x' : ''}] ${item.name}\n`)
+                }
+            }
+
+
+            if (c.links.length) {
+                await fs.appendFile(cardPath, '\n\n\n# Links:\n')
+                for (let j = 0; j < c.links.length; j++) {
+                    const link = c.links[j]
+                    if (link.external) {
+                        await fs.appendFile(cardPath, `* [${link.name}](${link.url})`)
+                    } else {
+                        await fs.appendFile(cardPath, `* [${link.name}](./${link.name})`)
+                        console.warn('Need to download', `${cardPath}/${c.slug}/${link.name}`, link.url)
+                        // const stream = require('fs').createWriteStream(`${OUTPUT}/${board.org}/${list.boardSlug}/${link.name}`)
+                        // require('https').get(link.url, res => res.pipe(stream))
+                    }
+
                 }
 
             }
-            await fs.appendFile(listPath, '~~~~~~~~~~~~~~~~~~~~\n')
+
+
         }
 
-        await fs.appendFile(listPath, '\n\n\n')
+
     }
+}
+
+const writeCard = async (card) => {
+
 }
 
 const getBoard = board => {
@@ -84,15 +117,41 @@ const getBoard = board => {
         pos: l.pos,
         closed: l.closed,
         cards: [],
-        boardSlug: slug
+        boardSlug: slug,
     })).sort((a, b) => a.pos > b.pos ? 1 : -1)
 
     const cards = getCards(board.cards);
 
+
     cards.forEach(c => {
+
+        c.checklistIds.forEach(cid => {
+            const list = board.checklists.find(checklist => checklist.id === cid);
+            c.checklists.push({
+                name: list.name,
+                pos: list.pos,
+                items: list.checkItems.map(item => ({
+                    state: item.state,
+                    name: item.name,
+                    pos: item.pos,
+                    due: item.due
+                })).sort((a, b) => a.pos > b.pos ? 1 : -1)
+            })
+
+            c.checklists = c.checklists.sort((a, b) => a.pos > b.pos ? 1 : -1)
+        })
+
         const list = lists.find(l => l.id === c.listId)
         list.cards.push(c)
     })
+
+    lists.forEach(l => {
+        l.cards = l.cards.map((c, i) => ({
+            ...c,
+            slug: `${c.closed ? '.' : ''}${(i + 1).toString().padStart(Math.log10(l.cards.length) + 1, '0')}${c.nonUniqueSlug}`
+        }))
+    })
+
 
     return {
         name: board.name,
@@ -104,13 +163,19 @@ const getBoard = board => {
 
 const getCards = (cards) => {
     return cards.map(c => {
+        const urlLastPart = c.url.substr(c.url.lastIndexOf('/') + 1);
+        //remove the card id, instead I'll use the pos index
+        const nonUniqueSlug = urlLastPart.substr(urlLastPart.indexOf('-'))
         return {
             name: c.name,
-            slug: c.url.substr(c.url.lastIndexOf('/') + 1),
+            nonUniqueSlug,
             closed: c.closed,
             body: c.desc,
             pos: c.pos,
             listId: c.idList,
+            checklistIds: c.idChecklists,
+            checklists: [],
+            modified: c.dateLastActivity,
             links: c.attachments.map(a => ({name: a.name, url: a.url, external: !a.isUpload})) //there's also a pos and mime type?
         }
     }).sort((a, b) => a.pos > b.pos ? 1 : -1)
